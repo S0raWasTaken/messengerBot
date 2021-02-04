ad6c76f7b9750913970a6657ba2c009d5728dacc
S0raWasTaken <anosensedroid@gmail.com>
2021-02-04 18:51:12 -0500

    added client.login()

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const client = require(".");
 const {date} = require("./handler");
+client.login(process.env.PAGE_ACCESS_TOKEN);
 
 client.on("ready", () => {
     console.log("[INFO: "+date+"] Bot logged in!");
