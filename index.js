ab1e5ba048a6b8178339e1cf79428428b2fa8c7c
S0raWasTaken <anosensedroid@gmail.com>
2021-02-06 08:56:44 -0500

    Added a ignored users list, so users that fail the filter will be ignored in the future

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const client = require("./app");
 const {date} = require("./handler");
 const filter = require("./filter");
+const ignored = new Set();
 client.login(process.env.PAGE_ACCESS_TOKEN);
 
 client.on("ready", () => {
@@ -11,7 +12,7 @@ client.on("ready", () => {
 client.on("message", message => {
     console.log("[INFO: "+date+"] Received message from API:\n"+message.content);
     const args = message.content.trim().split(/ +/g);
-    if (filter.read(args)) return;
+    if (filter.read(args)) return ignored.add(message.author.id);
 
     
 });
