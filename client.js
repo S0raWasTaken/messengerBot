4843b6555997dfaaf8eec372e843f93e9260b16d
S0raWasTaken <anosensedroid@gmail.com>
2021-02-07 14:28:32 -0500

    Removed Client.login() since now Client gets the token from .env

--- client.js
+++ client.js
@@ -5,15 +5,7 @@ const {date} = require("./handler");
 class Client extends EventEmitter {
     constructor() {
         super();
-        this.PAGE_ACCESS_TOKEN = "";
-    }
-
-    /**
-     * @param {String} token
-     * Needs to be a valid page access token
-     */
-    login(token) {
-        this.PAGE_ACCESS_TOKEN = token;
+        this.PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
     }
 
     /**
@@ -24,34 +16,41 @@ class Client extends EventEmitter {
      * @param {any} psid 
      * Person ID to send the message
      */
-    sendMessage(message, messaging_type, psid) {
+    sendMessage(message, psid) {
+        const messaging_type = "RESPONSE";
         if (this.PAGE_ACCESS_TOKEN.length < 1) {
             return console.error("[ERROR: "+date+"] INVALID_PAGE_ACCESS_TOKEN");
         }
-        if (isNaN(parseInt(psid))) {
-            return console.error("INVALID_PSID");
-        }
-        const messageTypes = ["RESPONSE", "UPDATE", "MESSAGE_TAG"];
-
+        //if (isNaN(parseInt(psid))) {
+        //    return console.error("INVALID_PSID");
+        //}
         if (message.length < 1) {
             return console.error("[ERROR: "+date+"] MESSAGE_TOO_SHORT");
         }
-        if (!messageTypes.find(type => type === messaging_type)) {
-            return console.error("[ERROR: "+date+"]INVALID_MESSAGE_TYPE");
-        }
         // Changed to my test app in localhost
-        const api = /*`https://graph.facebook.com/v9.0/me/messages?access_token=${this.PAGE_ACCESS_TOKEN}`*/ "localhost:25576";
+        const api = /*`https://graph.facebook.com/v9.0/me/messages?access_token=${this.PAGE_ACCESS_TOKEN}`*/ "localhost:25576/test";
 
-        const APIresponse = childProcess.exec(`curl -X POST -H "Content-type: application/json" -d '{\
-            "recipient": {"id":"${psid}"}, "message":{"text":"${message}"},\
-            }' "${api}"`, (error, stdout) => {
-            if (error) 
-                return console.error("[ERROR: "+date+"] "+error.message+"\n"+error.stack);
-            if (stdout) 
-                return stdout;
-        });
+        const APIresponse = send(psid, message, api);
         return APIresponse;
     }
 }
-
+/**
+ * @param {any} psid
+ * @param {String} message 
+ * @param {URL} api 
+ */
+function send(psid, message, api) {
+        var response;
+        childProcess.exec(`curl -X POST -H "Content-type: application/json" -d '{"recipient": {"id":"${psid}"}, "message":{"text":"${message}"} }' "${api}"`, 
+            (error, stdout) => {
+                if (error) 
+                    return console.error("[ERROR: "+date+"] "+error.message+"\n"+error.stack);
+                if (stdout) {
+                    response = stdout;
+                    return stdout;
+                }   
+            }
+        );
+        return response;
+}
 module.exports = Client;
